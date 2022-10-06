9fdb87f30f4244f970956c59cc0d98dc4f4c4029
GeniusAadi <107663742+GeniusAadi@users.noreply.github.com>
2022-10-06 12:22:40 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -6,15 +6,13 @@ for i in range(10000000000000000000000000000000000000000000000000000000000000000
     b = input("Enter an operator: ")
     c = float(input("Enter another number: "))
 #conditions for doing calculations
-    while(True):
-        if b == "+":
-            print(a+c)
-        elif b == "-":
-            print(a-c)
-        elif b == "x":
-            print(a*c)
-        elif b == "/":
-            print(a/c)  
-        else:
-            print("Please enter a valid operator!")
-        break      
+    if b == "+":
+        print(a+c)
+    elif b == "-":
+        print(a-c)
+    elif b == "x":
+        print(a*c)
+    elif b == "/":
+        print(a/c)  
+    else:
+        print("Error : Enter a valid operator!")
