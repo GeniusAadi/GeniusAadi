47fac23d4255b5f7ea455ff7776675064d2a7f29
GeniusAadi <107663742+GeniusAadi@users.noreply.github.com>
2022-11-19 22:19:25 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,19 +1,22 @@
-#Making a CUI calculator
-#Options for user
-print('Use operators: +,-,x,/')
-#start of the programme
+#Making a calculator
+#option for user
+print("Please enter any from given symbols: (+,-,x,/)")
 while True:
-    a = float(input("Enter a number: "))
-    b = input("Enter an operator: ")
-    c = float(input("Enter another number: "))
-#conditions for doing calculations
-    if b == "+":
-        print(a+c)
-    elif b == "-":
-        print(a-c)
-    elif b == "x":
-        print(a*c)
-    elif b == "/":
-        print(a/c)  
-    else:
-        print("Error : Enter a valid operator!")
+                try:
+                                a = float(input("Please enter your first number = " ))
+                                c = float(input("Please enter your second number = "))
+                                b = (input("Please enter your operator = " ))
+#conditions for calculation
+                                if b == "+":
+                                                print(a + c)
+                                elif b == "-":
+                                                print(a - c)
+                                elif b == "x":
+                                                print(a * c)              
+                                elif b == "/":
+                                                print(a / c)
+                                else:
+                                                print("Error : Please enter a valid operator!")  
+                 #for showing error if a and c are not numbers                                            
+                except ValueError:
+                                print("Error : Please enter a number!")                
