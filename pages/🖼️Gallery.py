2f35cc11302fa54518dd79994d696cc1ff113018
GeniusAadi <107663742+GeniusAadi@users.noreply.github.com>
2023-03-08 07:19:41 +0530

    Update 🖼️Gallery.py

--- pages/🖼️Gallery.py
+++ pages/🖼️Gallery.py
@@ -1,7 +1,14 @@
 import streamlit as st
+from PIL import Image
+from io import BytesIO
 from pathlib import Path
 
 st.set_page_config(page_title="Gallery", page_icon=":smile:", layout="wide")
+#function for downloading both videos and photos
+def download_image(url):
+    with open(url, "r+", encoding="latin1") as f:
+        url_bytes=f.read()
+        f.write(url_bytes)
 
 # get the website root directory
 root_dir = Path("my_directory")
@@ -18,14 +25,24 @@ if root_dir is not None:
 
     # get the list of files within the selected subdirectory
     files = [x for x in selected_dir.iterdir() if x.is_file()]
-    #st.progress(100)
-    #st.snow()
-    #st.balloons()
 
 # display the list of files
 st.write("Files in selected folder:")
 for file in files:
-    #st.image(file) or st.video(file)
-    #st.download_button("Download File",file)
-    #fbr=st.file_browser("Select a file")
     st.write("- " + file.name)
+    if selected_dir == subdirs[0]:
+        image_paths= Image.open(file)
+        with BytesIO() as output:
+            image_paths.save(output, format='PNG')
+            image_bytes = output.getvalue()
+        if st.download_button("Download Image",image_bytes,file_name=file.name,mime="png/jpg/pdf/gif/tiff/psd/eps/ai/indd/raw"):
+            download_image(file)
+            st.success("Image downloaded successfully")
+        st.image(image_paths, width=250, caption=image_paths)
+    elif selected_dir == subdirs[1]:
+        video_file = open(file, 'rb')
+        video_bytes = video_file.read()
+        if st.download_button("Download Video",video_bytes,file_name=file.name,mime="webm/mpg/mp2/mpeg/mpe/mpv/ogg/mp4/m4p/m4v/avi/wmv/mov/qt/flv/swf/avchd"):
+            download_image(file)
+            st.success("Video downloaded successfully")
+        st.video(video_bytes)
