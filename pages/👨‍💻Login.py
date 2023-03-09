27b30e02189a21f7c06caa3d1a54c526390a49f6
GeniusAadi <107663742+GeniusAadi@users.noreply.github.com>
2023-03-09 11:10:52 +0530

    Update 👨‍💻Login.py

--- pages/👨‍💻Login.py
+++ pages/👨‍💻Login.py
@@ -40,6 +40,17 @@ def store():
                 st.video(upload)
             st.success("File Uploaded Successfully.")
             #switch_page("Gallery")
+# Function to delete selected files
+def delete_selected_files(selected_files):
+    # Delete selected files from the "photos" directory
+    if os.listdir("my_directory/photos"):
+        for file in selected_files:
+            os.remove(os.path.join("my_directory/photos", file))
+    # Delete selected files from the "videos" directory
+    else:
+        for file in selected_files:
+            os.remove(os.path.join("my_directory/videos", file))
+    st.success("Selected files have been deleted.")
 st.title("Login as Admin")
 name=st.text_input("Admin name")
 password=st.text_input("Password",type='password')
@@ -61,5 +72,26 @@ if (name=="Rakes Rao") and (password=="R@kE$#M@th$&Mu$ic"):
             pho=st.checkbox("Photos")
             vdo=st.checkbox("Videos")
             store()
+            rm=st.button("Delete")
+            #initialize session state
+            if "rm_state" not in st.session_state:
+                st.session_state.rm_state = False
+            if rm or st.session_state.lg_state:
+                st.session_state.rm_state = True
+                # Display checkboxes for all files in the "photos" directory
+                photos_files = os.listdir("my_directory/photos")
+                photos_files_selected = st.multiselect("Select photos to delete:", options=photos_files)
+
+                # Display checkboxes for all files in the "videos" directory
+                videos_files = os.listdir("my_directory/videos")
+                videos_files_selected = st.multiselect("Select videos to delete:", options=videos_files)
+
+                # Create a button to delete selected files
+                if st.button("Delete Selected Files"):
+                    if len(photos_files_selected) == 0 and len(videos_files_selected) == 0:
+                        st.warning("No files selected.")
+                    else:
+                        selected_files = photos_files_selected + videos_files_selected
+                        delete_selected_files(selected_files)
 else:
     st.header("You are not admin :angry:.")
