0cdfd4cb0fb64120369c2c86ed620adacc53be59
GeniusAadi <107663742+GeniusAadi@users.noreply.github.com>
2023-07-09 01:46:44 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -2,21 +2,23 @@
 #option for user
 print("Please enter any from given operators: (+,-,x,/)")
 while True:
-                try:
-                                a = float(input("Please enter your first number = " ))
-                                c = float(input("Please enter your second number = "))
-                                b = (input("Please enter your operator = " ))
+  try:
+    a = float(input("Please enter your first number = " ))
+    c = float(input("Please enter your second number = "))
+    b = (input("Please enter your operator = " ))
 #conditions for calculation
-                                if b == "+":
-                                                print(a + c)
-                                elif b == "-":
-                                                print(a - c)
-                                elif b == "x":
-                                                print(a * c)              
-                                elif b == "/":
-                                                print(a / c)
-                                else:
-                                                print("Error : Please enter a valid operator!")  
-                 #for showing error if a and c are not numbers                                            
-                except Exception as e:
-                                print("Error : Please enter a number!")                
+    if b == "+":
+      print(a + c)
+    elif b == "-":
+      print(a - c)
+    elif b == "x":
+      print(a * c)              
+    elif b == "/":
+      print(a / c)
+    else:
+      print("Error : Please enter a valid operator!")  
+#for showing error if a and c are not numbers
+  except ZeroDivisionError:
+    print("Error : Please enter a non-zero number!")
+  except Exception as e:
+    print("Error : Please enter a number!")                
