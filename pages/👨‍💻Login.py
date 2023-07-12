a10739adf665686d67d0999cfb2314d1054940a5
GeniusAadi <107663742+GeniusAadi@users.noreply.github.com>
2023-07-12 21:22:08 +0530

    Update 👨‍💻Login.py

--- pages/👨‍💻Login.py
+++ pages/👨‍💻Login.py
@@ -1,158 +1,124 @@
 import streamlit as st
 import base64
-#from streamlit_extras.switch_page_button import switch_page
-import os
-#To connect github and upload
 import requests
-# GitHub repository information
-owner = "GeniusAadi"
-repo = "GeniusAadi"
-branch = "main" 
-#file_path = "path/to/your/file.txt"  # path to the file you want to upload
-commit_message = "Upload file"
-#Setting page heading
-st.set_page_config(page_title="Admin Login", page_icon=':page_with_curl:', layout="wide")
-hide_st_style = '''
-    <style>
-    #MainMenu {visibility: hidden;}
-    footer {visibility: hidden;}
-    header {visibility: hidden;}
-    </style>
-    '''
-st.markdown(hide_st_style, unsafe_allow_html=True)
-#function to upload photos and videos
-def file():
-    global upload
-    if pho:
-        upload = st.file_uploader("Upload Photos",type=["png","jpg","pdf","gif","tiff","psd","eps","ai","indd","raw"])
-        if upload is not None:
-            return upload
-    elif vdo:
-        upload = st.file_uploader("Upload Videos",type=["webm","mpg","mp2","mpeg","mpe","mpv","ogg","mp4","m4p","m4v","avi","wmv","mov","qt","flv","swf","avchd"])
-        if upload is not None:
-            return upload
-#function to store photos and videos
-def store():
-    global upload
-    upload = file()
-    upd = st.button("Upload")
+import os
 
-    # Initialize session state
-    if "upd_state" not in st.session_state:
-        st.session_state.upd_state = False
+class GalleryApp:
+    def __init__(self, owner, repo, branch, personal_access_token):
+        self.owner = owner
+        self.repo = repo
+        self.branch = branch
+        self.personal_access_token = personal_access_token
 
-    if upd or st.session_state.upd_state:
-        st.session_state.upd_state = True
+    def login(self):
+        # Login form
+        st.title("Login as Admin")
+        name = st.text_input("Admin name")
+        password = st.text_input("Password", type='password')
+        if (name == "Rakes Rao") and (password == "R@kE$#M@th$&Mu$ic"):
+            self.show_gallery()
 
-        if upload is not None:
-            file_content = upload.getvalue()
-            file_content_base64 = base64.b64encode(file_content).decode("utf-8")
-            file_path = f"my_directory/{'photos' if pho else 'videos'}/{upload.name}"
-            commit_message = "Upload file"
-
-            # Create the file payload
-            payload = {
-                "message": commit_message,
-                "content": file_content_base64,
-                "branch": branch
-            }
-
-            # Create the file on GitHub
-            url = f"https://api.github.com/repos/{owner}/{repo}/contents/{file_path}"
-            headers = {"Authorization": f"token {personal_access_token}"}
-            response = requests.put(url, json=payload, headers=headers)
-
-            if response.status_code == 201:
-                st.success("File uploaded successfully.")
-                st.image(upload) if pho else st.video(upload)
-            else:
-                st.error("Failed to upload file.")
-                st.error(f"Response: {response.text}")
-
-            #switch_page("Gallery")
-# Function to delete selected files
-def delete_selected_files(selected_files):
-    if len(selected_files) == 0:
-        st.warning("No files selected.")
-        return
-
-    # Delete selected files from GitHub
-    for file in selected_files:
-        file_path = f"my_directory/photos/{file}" if os.listdir("my_directory/photos") else f"my_directory/videos/{file}"
-        commit_message = "Delete file"
-
-        # Get the file's current SHA
-        url = f"https://api.github.com/repos/{owner}/{repo}/contents/{file_path}"
-        headers = {"Authorization": f"token {personal_access_token}"}
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            file_data = response.json()
-            file_sha = file_data["sha"]
-
-            # Create the deletion payload
-            payload = {
-                "message": commit_message,
-                "sha": file_sha,
-                "branch": branch
-            }
-
-            # Delete the file on GitHub
-            delete_url = f"https://api.github.com/repos/{owner}/{repo}/contents/{file_path}"
-            delete_response = requests.delete(delete_url, json=payload, headers=headers)
-
-            if delete_response.status_code == 200:
-                st.success(f"File '{file}' deleted successfully.")
-            else:
-                st.error(f"Failed to delete file '{file}'.")
-                st.error(f"Response: {delete_response.text}")
-        else:
-            st.error(f"Failed to retrieve file data for '{file}'.")
-            st.error(f"Response: {response.text}")
-
-    st.success("Selected files have been deleted.")
-
-#Login form
-st.title("Login as Admin")
-name=st.text_input("Admin name")
-password=st.text_input("Password",type='password')
-if (name=="Rakes Rao") and (password=="R@kE$#M@th$&Mu$ic"):
-    lg=st.button("Login")
-    #initialize session state
-    if "lg_state" not in st.session_state:
-        st.session_state.lg_state = False
-    if lg or st.session_state.lg_state:
-        st.session_state.lg_state = True
+    def show_gallery(self):
         st.title("Welcome to Gallery.")
         st.header("WELCOME Rao Sir.")
-        gal=st.button("Gallery")
-        #initialize session state
-        if "gal_state" not in st.session_state:
-            st.session_state.gal_state = False
-        if gal or st.session_state.gal_state:
-            st.session_state.gal_state = True
-            pho=st.checkbox("Photos")
-            vdo=st.checkbox("Videos")
-            store()
-            rm=st.button("Delete")
-            #initialize session state
-            if "rm_state" not in st.session_state:
-                st.session_state.rm_state = False
-            if rm or st.session_state.lg_state:
-                st.session_state.rm_state = True
-                # Display checkboxes for all files in the "photos" directory
-                photos_files = os.listdir("my_directory/photos")
-                photos_files_selected = st.multiselect("Select photos to delete:", options=photos_files)
+        gal = st.button("Gallery")
+        if gal:
+            self.display_files()
+            self.upload_files()
+            self.delete_files()
 
-                # Display checkboxes for all files in the "videos" directory
-                videos_files = os.listdir("my_directory/videos")
-                videos_files_selected = st.multiselect("Select videos to delete:", options=videos_files)
+    def display_files(self):
+        pho = st.checkbox("Photos")
+        vdo = st.checkbox("Videos")
+        if pho:
+            photos_files = os.listdir("my_directory/photos")
+            st.write("Photos:")
+            for file in photos_files:
+                st.write("- " + file)
+        if vdo:
+            videos_files = os.listdir("my_directory/videos")
+            st.write("Videos:")
+            for file in videos_files:
+                st.write("- " + file)
 
-                # Create a button to delete selected files
-                if st.button("Delete Selected Files"):
-                    if len(photos_files_selected) == 0 and len(videos_files_selected) == 0:
-                        st.warning("No files selected.")
-                    else:
-                        selected_files = photos_files_selected + videos_files_selected
-                        delete_selected_files(selected_files)
-else:
-    st.header("You are not admin :angry:.")
+    def upload_files(self):
+        pho = st.checkbox("Photos")
+        vdo = st.checkbox("Videos")
+        if pho or vdo:
+            upload = st.file_uploader("Upload Files", type=["png", "jpg", "pdf", "gif", "tiff", "psd", "eps", "ai", "indd", "raw", "webm", "mpg", "mp2", "mpeg", "mpe", "mpv", "ogg", "mp4", "m4p", "m4v", "avi", "wmv", "mov", "qt", "flv", "swf", "avchd"])
+            if upload is not None:
+                file_content = upload.getvalue()
+                file_content_base64 = base64.b64encode(file_content).decode("utf-8")
+                file_path = f"my_directory/{'photos' if pho else 'videos'}/{upload.name}"
+                commit_message = "Upload file"
+
+                payload = {
+                    "message": commit_message,
+                    "content": file_content_base64,
+                    "branch": self.branch
+                }
+
+                url = f"https://api.github.com/repos/{self.owner}/{self.repo}/contents/{file_path}"
+                headers = {"Authorization": f"token {self.personal_access_token}"}
+                response = requests.put(url, json=payload, headers=headers)
+
+                if response.status_code == 201:
+                    st.success("File uploaded successfully.")
+                    st.image(upload) if pho else st.video(upload)
+                else:
+                    st.error("Failed to upload file.")
+                    st.error(f"Response: {response.text}")
+
+    def delete_files(self):
+        pho = st.checkbox("Photos")
+        vdo = st.checkbox("Videos")
+        if pho or vdo:
+            files = []
+            if pho:
+                files.extend(os.listdir("my_directory/photos"))
+            if vdo:
+                files.extend(os.listdir("my_directory/videos"))
+
+            selected_files = st.multiselect("Select files to delete:", options=files)
+            if st.button("Delete Selected Files"):
+                if len(selected_files) == 0:
+                    st.warning("No files selected.")
+                else:
+                    for file in selected_files:
+                        file_path = f"my_directory/{'photos' if pho else 'videos'}/{file}"
+                        commit_message = "Delete file"
+
+                        url = f"https://api.github.com/repos/{self.owner}/{self.repo}/contents/{file_path}"
+                        headers = {"Authorization": f"token {self.personal_access_token}"}
+                        response = requests.get(url, headers=headers)
+
+                        if response.status_code == 200:
+                            file_data = response.json()
+                            file_sha = file_data["sha"]
+
+                            payload = {
+                                "message": commit_message,
+                                "sha": file_sha,
+                                "branch": self.branch
+                            }
+
+                            delete_url = f"https://api.github.com/repos/{self.owner}/{self.repo}/contents/{file_path}"
+                            delete_response = requests.delete(delete_url, json=payload, headers=headers)
+
+                            if delete_response.status_code == 200:
+                                st.success(f"File '{file}' deleted successfully.")
+                            else:
+                                st.error(f"Failed to delete file '{file}'.")
+                                st.error(f"Response: {delete_response.text}")
+                        else:
+                            st.error(f"Failed to retrieve file data for '{file}'.")
+                            st.error(f"Response: {response.text}")
+
+if __name__ == "__main__":
+    owner = "GeniusAadi"
+    repo = "GeniusAadi"
+    branch = "main"
+    personal_access_token = "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"
+
+    app = GalleryApp(owner, repo, branch, personal_access_token)
+    app.login()
