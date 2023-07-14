ad51f0a8f64c6a86548f18244179fe22632124d7
GeniusAadi <107663742+GeniusAadi@users.noreply.github.com>
2023-07-14 22:08:13 +0530

    Update 👨‍💻Login.py

--- pages/👨‍💻Login.py
+++ pages/👨‍💻Login.py
@@ -83,7 +83,7 @@ class GalleryApp:
                 files.extend(os.listdir("my_directory/photos"))
             if vdo:
                 files.extend(os.listdir("my_directory/videos"))
-
+    
             selected_files = st.multiselect("Select files to delete:", options=files, key="delete_files_multiselect")
             if st.button("Delete Selected Files", key="delete_files_button"):
                 if len(selected_files) == 0:
@@ -93,24 +93,24 @@ class GalleryApp:
                         file_dir = "photos" if file in files[:len(files)//2] else "videos"
                         file_path = f"my_directory/{file_dir}/{file}"
                         commit_message = "Delete file"
-
+    
                         url = f"https://api.github.com/repos/{self.owner}/{self.repo}/contents/{file_path}"
                         headers = {"Authorization": f"token {self.personal_access_token}"}
                         response = requests.get(url, headers=headers)
-
+    
                         if response.status_code == 200:
                             file_data = response.json()
                             file_sha = file_data["sha"]
-
+    
                             payload = {
                                 "message": commit_message,
                                 "sha": file_sha,
                                 "branch": self.branch
                             }
-
+    
                             delete_url = f"https://api.github.com/repos/{self.owner}/{self.repo}/contents/{file_path}"
                             delete_response = requests.delete(delete_url, json=payload, headers=headers)
-
+    
                             if delete_response.status_code == 200:
                                 st.success(f"File '{file}' deleted successfully.")
                             else:
