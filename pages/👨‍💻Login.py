afb24e0e9f6e93cb835990a031a320a2ec447149
GeniusAadi <107663742+GeniusAadi@users.noreply.github.com>
2023-07-14 21:42:15 +0530

    Update 👨‍💻Login.py

--- pages/👨‍💻Login.py
+++ pages/👨‍💻Login.py
@@ -73,50 +73,50 @@ class GalleryApp:
                     st.error(f"Response: {response.text}")
 
     def delete_files(self):
-    pho = st.checkbox("Photos")
-    vdo = st.checkbox("Videos")
-    if pho or vdo:
-        files = []
-        if pho:
-            files.extend(os.listdir("my_directory/photos"))
-        if vdo:
-            files.extend(os.listdir("my_directory/videos"))
-
-        selected_files = st.multiselect("Select files to delete:", options=files)
-        if st.button("Delete Selected Files"):
-            if len(selected_files) == 0:
-                st.warning("No files selected.")
-            else:
-                for file in selected_files:
-                    file_dir = "photos" if file in files[:len(files)//2] else "videos"
-                    file_path = f"my_directory/{file_dir}/{file}"
-                    commit_message = "Delete file"
-
-                    url = f"https://api.github.com/repos/{self.owner}/{self.repo}/contents/{file_path}"
-                    headers = {"Authorization": f"token {self.personal_access_token}"}
-                    response = requests.get(url, headers=headers)
-
-                    if response.status_code == 200:
-                        file_data = response.json()
-                        file_sha = file_data["sha"]
-
-                        payload = {
-                            "message": commit_message,
-                            "sha": file_sha,
-                            "branch": self.branch
-                        }
-
-                        delete_url = f"https://api.github.com/repos/{self.owner}/{self.repo}/contents/{file_path}"
-                        delete_response = requests.delete(delete_url, json=payload, headers=headers)
-
-                        if delete_response.status_code == 200:
-                            st.success(f"File '{file}' deleted successfully.")
+        pho = st.checkbox("Photos")
+        vdo = st.checkbox("Videos")
+        if pho or vdo:
+            files = []
+            if pho:
+                files.extend(os.listdir("my_directory/photos"))
+            if vdo:
+                files.extend(os.listdir("my_directory/videos"))
+    
+            selected_files = st.multiselect("Select files to delete:", options=files)
+            if st.button("Delete Selected Files"):
+                if len(selected_files) == 0:
+                    st.warning("No files selected.")
+                else:
+                    for file in selected_files:
+                        file_dir = "photos" if file in files[:len(files)//2] else "videos"
+                        file_path = f"my_directory/{file_dir}/{file}"
+                        commit_message = "Delete file"
+    
+                        url = f"https://api.github.com/repos/{self.owner}/{self.repo}/contents/{file_path}"
+                        headers = {"Authorization": f"token {self.personal_access_token}"}
+                        response = requests.get(url, headers=headers)
+    
+                        if response.status_code == 200:
+                            file_data = response.json()
+                            file_sha = file_data["sha"]
+    
+                            payload = {
+                                "message": commit_message,
+                                "sha": file_sha,
+                                "branch": self.branch
+                            }
+    
+                            delete_url = f"https://api.github.com/repos/{self.owner}/{self.repo}/contents/{file_path}"
+                            delete_response = requests.delete(delete_url, json=payload, headers=headers)
+    
+                            if delete_response.status_code == 200:
+                                st.success(f"File '{file}' deleted successfully.")
+                            else:
+                                st.error(f"Failed to delete file '{file}'.")
+                                st.error(f"Response: {delete_response.text}")
                         else:
-                            st.error(f"Failed to delete file '{file}'.")
-                            st.error(f"Response: {delete_response.text}")
-                    else:
-                        st.error(f"Failed to retrieve file data for '{file}'.")
-                        st.error(f"Response: {response.text}")
+                            st.error(f"Failed to retrieve file data for '{file}'.")
+                            st.error(f"Response: {response.text}")
 
     def commit_and_push_changes(self, commit_message):
         repo = Repo(self.local_repo_path)
